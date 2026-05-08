cc08ba86b20dacc2b66fa113bb4e1926969f44b5
vyphan187 <vyphan187@gmail.com>
2026-05-09 01:27:23 +0700

    Add files via upload

---
+++ Backend_API/PhanCongCaController.cs
@@ -0,0 +1,83 @@
+﻿using MasanCoffee.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MasanCoffee.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhanCongCaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PhanCongCaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+       
+        [HttpGet]
+        public async Task<IActionResult> GetLichPhanCong()
+        {
+            var lich = await _context.PhanCongCa
+                .Include(p => p.NhanVien)
+                .Select(p => new {
+                    p.MaPhanCong,
+                    p.NgayLam,
+                    p.MaNhanVien,
+                    
+                    hoTenNhanVien = p.NhanVien.Ho + " " + p.NhanVien.Ten,
+                    p.MaCa,
+                    p.SoGioLam
+                }).ToListAsync();
+
+            return Ok(lich);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostPhanCong(PhanCongCa pc)
+        {
+            try
+            {
+          
+                bool biTrungLich = await _context.PhanCongCa.AnyAsync(x =>
+                    x.NgayLam.Date == pc.NgayLam.Date &&
+                    x.MaNhanVien == pc.MaNhanVien &&
+                    x.MaCa == pc.MaCa);
+
+                if (biTrungLich)
+                {
+                   
+                    return Conflict(new { thongBao = "Nhân viên này đã được phân vào ca này trong ngày hôm nay rồi!" });
+                }
+
+                
+                var phanCongMoi = new PhanCongCa
+                {
+                    NgayLam = pc.NgayLam,
+                    MaNhanVien = pc.MaNhanVien,
+                    MaCa = pc.MaCa,
+                    SoGioLam = pc.SoGioLam 
+                };
+
+                _context.PhanCongCa.Add(phanCongMoi);
+                await _context.SaveChangesAsync();
+                return Ok(new { thongBao = "Lưu lịch làm việc thành công!" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { thongBao = "Lỗi hệ thống: " + (ex.InnerException?.Message ?? ex.Message) });
+            }
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhanCong(int id)
+        {
+            var pc = await _context.PhanCongCa.FindAsync(id);
+            if (pc == null) return NotFound(new { thongBao = "Không tìm thấy lịch phân công này!" });
+
+            _context.PhanCongCa.Remove(pc);
+            await _context.SaveChangesAsync();
+            return Ok(new { thongBao = "Đã xóa lịch phân công thành công!" });
+        }
+    }
+}
