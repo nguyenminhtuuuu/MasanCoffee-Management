01831db447882ee38bb61a7f6eb5995a7368bdc3
vyphan187 <vyphan187@gmail.com>
2026-05-09 22:48:01 +0700

    Add files via upload

---
+++ Frontend_Angular/src/app/services/nhan-su.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NhanVien } from '../models/nhan-vien.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NhanSuService {
+
+  private apiUrl = 'https://localhost:7219/api/NhanVien';
+
+  constructor(private http: HttpClient) { }
+
+  getDanhSachNhanVien(): Observable<NhanVien[]> {
+    return this.http.get<NhanVien[]>(this.apiUrl);
+  }
+
+  getLichPhanCong(): Observable<any[]> {
+    return this.http.get<any[]>('https://localhost:7219/api/PhanCongCa');
+  }
+  themNhanVien(nv: any): Observable<any> {
+    return this.http.post('https://localhost:7219/api/NhanVien', nv);
+  }
+
+  capNhatNhanVien(id: any, data: any): Observable<any> {
+    return this.http.put(`https://localhost:7219/api/NhanVien/${id}`, data);
+  }
+
+  xoaNhanVien(id: any): Observable<any> {
+    return this.http.delete(`https://localhost:7219/api/NhanVien/${id}`);
+  }
+  
+luuPhanCong(data: any): Observable<any> {
+  return this.http.post('https://localhost:7219/api/PhanCongCa', data);
+}
+
+
+}
